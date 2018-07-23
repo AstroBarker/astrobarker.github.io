42e5949ed4fad41d2c609f497f5d36eb08d15011
Brandon Barker <bbarker.py@protonmail.ch>
2018-07-23 12:01:32 -0400

    WIP

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "MidSure Poster Presentation",
+        "excerpt":" ","categories": ["Research"],
+        "tags": [],
+        "url": "http://localhost:4000/research/midsure/",
+        "teaser":null}]
