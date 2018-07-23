cd2e89fc7d38449d702150950e0c6d871120629f
Brandon Barker <bbarker.py@protonmail.ch>
2018-07-23 12:07:32 -0400

    WIP

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,4 +1,9 @@
 var store = [{
+        "title": "Midsure/index",
+        "excerpt":"      MidSure Poster Presentation - Brandon Barker                                            Brandon Barker                                                        About                                                            Outreach                                                            CV                                                            Blog                                                        Toggle Menu                                                                                               Follow:                       GitHub                 Feed  &copy; 2018 Brandon Barker. Powered by Jekyll &amp; Minimal Mistakes.                    ","categories": ["astrobarker.github.io"],
+        "tags": [],
+        "url": "http://localhost:4000/astrobarker.github.io/midsure-index/",
+        "teaser":null},{
         "title": "MidSure Poster Presentation",
         "excerpt":" ","categories": ["Research"],
         "tags": [],
